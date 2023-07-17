d773066094d47ffff47e543264ea294e0811c407
Amphyby <amphyby@protonmail.com>
2023-07-17 02:43:08 +0200

    TestCellsDeletionAdjacent OK

--- main.cpp
+++ main.cpp
@@ -596,9 +596,9 @@ int main() {
     RUN_TEST(tr, TestFormulaInvalidPosition);
     RUN_TEST(tr, TestCellErrorPropagation);
     RUN_TEST(tr, TestCellsDeletionSimple);
-    /*RUN_TEST(tr, TestCellsDeletion);
+    RUN_TEST(tr, TestCellsDeletion);
     RUN_TEST(tr, TestCellsDeletionAdjacent);
-    RUN_TEST(tr, TestPrint);
+    /*RUN_TEST(tr, TestPrint);
     RUN_TEST(tr, TestCellReferences);
     RUN_TEST(tr, TestFormulaIncorrect);
     RUN_TEST(tr, TestCellCircularReferences);*/
